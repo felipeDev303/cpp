46d997beeee92777ac35b50c04bc56b305df0dc9
felipeDev303 <felipe.morales.roa@ciisa.cl>
2026-02-17 02:00:22 -0300

    Refactor code structure for improved readability and maintainability

---
+++ Fundamentos de la programación en C++/Introducción a la sintaxis y estructura en C++/helloworld.cpp
@@ -0,0 +1,19 @@
+// Incluimos la librería estándar de flujo de entrada-salida para ingresar y mostrar datos 
+#include <iostream>
+
+//Main function, punto de entrada a la app
+int main(){
+    std::cout << "¡Hola Mundo!" << std::endl;
+    return 0; 
+
+}
+
+/*
+std::cout: objeto estándar para enviar datos a la consola
+
+<<: operador de inserción del flujo, dirige la salida al flujo
+
+"¡Hola Mundo!": string que deseo mostrar
+
+<< std::endl;: nueva línea espues del output para formatear
+*/
